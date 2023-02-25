d1af3cee3cf6cd3793e3efb8c5d3d5f214441bbd
chernigin.y@gmail.com <chernigin.y@gmail.com>
2023-02-25 19:30:02 +0000

    model-fact_fhv_trips: implement first version of model

---
+++ models/core/fact_fhv_trips.sql
@@ -0,0 +1,37 @@
+{{ config(
+        matearilized='table',
+        partition_by={
+            'field': 'pickup_datetime',
+            'data_type': 'timestamp',
+            'granularity': 'day'
+        }
+    )
+}}
+
+
+with dim_zones as (
+
+    select location_id, borough, zone
+    from {{ ref('dim_zones') }}
+
+)
+
+select 
+    trips.service_type,
+    trips.trip_id,
+    trips.pickup_datetime,
+    trips.dropoff_datetime,
+    pickup_zone.borough as pickup_borough,
+    pickup_zone.zone as pickup_zone,
+    dropoff_zone.borough as dropoff_borough,
+    dropoff_zone.zone as dropoff_zone,
+    trips.is_shared_trip,
+    trips.dispatching_base_number,
+    trips.affiliated_base_number
+from {{ ref('stg_fhv_taxi_trips') }} as trips
+
+left join dim_zones pickup_zone
+      on pickup_zone.location_id = trips.pickup_location_id
+      
+left join dim_zones dropoff_zone
+      on dropoff_zone.location_id = trips.dropoff_location_id
--- models/stage/stg_fhv_taxi_trips.sql
+++ models/stage/stg_fhv_taxi_trips.sql
@@ -4,19 +4,15 @@
 
 select 
     'FHV' as service_type,
-    {{ dbt_utils.generate_surrogate_key([
-            'pickup_datetime', 'dropOff_datetime',
-            'PUlocationID', 'DOlocationID', 
-            'dispatching_base_num'
-    ]) }} as trip_id,
-    trips.pickup_datetime,
-    trips.dropOff_datetime as dropoff_datetime,
-    trips.PUlocationID as pickup_location_id,
-    trips.DOlocationID as dropoff_location_id,
-    trips.SR_Flag as is_shared_trip,
-    trips.dispatching_base_num as dispatching_base_number,
-    trips.Affiliated_base_number as affiliated_base_number
-from {{ source('stage', 'fhv_taxi_trips_external') }} trips
+    {{ dbt_utils.generate_surrogate_key(['pickup_datetime', 'dropOff_datetime', 'PUlocationID', 'DOlocationID']) }} as trip_id,
+    pickup_datetime,
+    dropOff_datetime as dropoff_datetime,
+    PUlocationID as pickup_location_id,
+    DOlocationID as dropoff_location_id,
+    SR_Flag as is_shared_trip,
+    dispatching_base_num as dispatching_base_number,
+    Affiliated_base_number as affiliated_base_number
+from {{ source('stage', 'fhv_taxi_trips_external') }} as trips
 {% if var('is_test_run', default=false) %}
 
     limit 100
